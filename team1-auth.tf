a40a63c98a9d605649fca3ad08f1cccf41e5628c
excircle <unixdisciple.us@gmail.com>
2022-03-30 18:34:48 +0000

    Create svc act enabled gcp mount (1)

--- team1-auth.tf
+++ team1-auth.tf
@@ -1,7 +1,9 @@
-/*
 resource "vault_auth_backend" "gcp" {
     provider = vault.team1_namespace
     path = "gcp"
     type = "gcp"
+    client_id = "108986691207062336156"
+    private_key_id = "806f949f29c8069df40a131cba7d4f81402786d7"
+    project_id = "hc-6eded284b47d419096eb99dd00f"
+    client_email = "tf-test@hc-6eded284b47d419096eb99dd00f.iam.gserviceaccount.com"
 }
-*/
