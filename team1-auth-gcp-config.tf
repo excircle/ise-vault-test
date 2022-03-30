4e7e93d6fe9792b0e6d42dd7cd9cc23f1e6b954e
excircle <unixdisciple.us@gmail.com>
2022-03-30 18:23:41 +0000

    Undo gcp config (1)

--- team1-auth-gcp-config.tf
+++ team1-auth-gcp-config.tf
@@ -1,3 +1,4 @@
+/*
 resource "vault_gcp_auth_backend_role" "gcp" {
     provider               = vault.team1_namespace
     backend                = vault_auth_backend.gcp.path
@@ -6,3 +7,4 @@ resource "vault_gcp_auth_backend_role" "gcp" {
     bound_service_accounts = ["tf-test@hc-6eded284b47d419096eb99dd00f.iam.gserviceaccount.com"]
     token_policies         = ["team1-admin"]
 }
+*/
