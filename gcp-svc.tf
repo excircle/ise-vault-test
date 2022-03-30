3fe58dcf0ee07bfe0621f8f0a79c034bb975d7aa
excircle <unixdisciple.us@gmail.com>
2022-03-30 15:16:44 +0000

    Create GCP Service account (1)

---
+++ gcp-svc.tf
@@ -0,0 +1,23 @@
+resource "google_service_account" "tf_test" {
+  account_id   = "tf-test"
+  display_name = "tf-test"
+}
+
+variable "google_service_account_iam_roles" {
+  type        = list(string)
+  description = "List of IAM roles to give to service account"
+  default = [
+    "roles/iam.securityAdmin,
+    "roles/iam.securityReviewer,
+    "roles/owner",
+    "roles/resourcemanager.projectIamAdmin"
+  ]
+}
+
+resource "google_project_iam_member" "vault_iam" {
+  for_each = toset(var.google_service_account_iam_roles)
+
+  project = "hc-6eded284b47d419096eb99dd00f"
+  role    = each.value
+  member  = format("serviceAccount:%s", google_service_account.tf_test.email)
+}
--- providers.tf
+++ providers.tf
@@ -1,4 +1,19 @@
-// Root provider 2
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "4.14.0"
+    }
+  }
+
+}
+
+provider "google" {
+  project = "hc-6eded284b47d419096eb99dd00f"
+  region  = "us-west1"
+}
+
+// Root provider 
 provider "vault" {
   alias = "root_namespace"
 }
