b6dfa93d2a2b83dd4da463bd5555ad0509d4dc76
excircle <unixdisciple.us@gmail.com>
2022-03-30 18:09:44 +0000

    Create gcp auth role (4)

--- team1-auth-gcp-config.tf
+++ team1-auth-gcp-config.tf
@@ -3,7 +3,6 @@ resource "vault_gcp_auth_backend_role" "gcp" {
     backend                = vault_auth_backend.gcp.path
     type                   = "iam"
     role                   = "roles/iam.serviceAccountKeyAdmin"
-    project_id             = "hc-6eded284b47d419096eb99dd00f"
     bound_service_accounts = ["tf-test@hc-6eded284b47d419096eb99dd00f.iam.gserviceaccount.com"]
     token_policies         = ["team1-admin"]
 }
